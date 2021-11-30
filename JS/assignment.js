2943ed029b5a6055dfbfb66da3cbe277d71824a0
thantthureinhtut <treinhtut@gmail.com>
2021-11-30 17:59:34 +0630

    Assignments

---
+++ JS/assignment.js
@@ -0,0 +1,120 @@
+var num1 = 10
+var num2 = 25
+var result = num1 + num2
+ console.log(result)
+
+ var num3 = 10
+ var num4 =25
+ var result = num4 - num3
+ console.log(result)
+
+ var num5 = 10
+ var num6 = 25
+ var result = num5 * num6
+ console.log(result)
+
+ var num7 = 10
+ var num8 = 25
+ var result = num7 / num8
+ console.log(result)
+
+ var num9 = 10
+ var num10 = 25
+ var result = num10 % num9
+ console.log (result)
+
+ var a = 10
+ var b = 25
+ var result = a += b
+ console.log(result)
+
+ var c = 10
+ var d = 25
+ var result = d -= c
+ console.log(result)
+
+ var e = 10
+ var f = 25
+ var result = e *= f
+ console.log(result)
+
+ var g = 10
+ var h = 25
+ var result = g /= h
+ console.log(result)
+
+ var i = 10
+ var j = 25
+ var result = i %= j
+ console.log(result)
+
+ var _1 = 10
+ var _2 = 25
+ var result = _1 < _2
+ console.log(result)
+
+ var _3 = 10
+ var _4 = 25
+ var result = _3 > _4
+ console.log(result)
+
+// <=  >=
+
+ var _4 = 10
+ var _5 = 25
+ var result = _4 <= _5
+ console.log(result)
+
+ var _6 = 10
+ var _7 = 25
+ var result = _6 >= _7
+ console.log(result)
+
+ var Test1 = 20
+var Test2 = 40
+if(Test1 <= Test2){
+    console.log("That answer is correct")
+}else {
+    console.log("That answer is not correct")
+}
+if(Test1 >= Test2){
+    console.log("That answer is correct")
+}else {
+    console.log("That answer is not correct")
+}
+// ------------------------------------
+ var _8 = 10
+ var _9 = 25
+ var result = _8 != _9
+ console.log(result)
+
+var $1 = 10
+var $2 = "25"
+console.log($1 == $2)
+
+var $3 = 10
+var $4 = "25"
+console.log ($3 === $4)
+// -------------------------------------
+// ==
+ var test1 = 20
+ var test2 = 40
+ if( test1 == test2 ){
+    console.log( "That answer is correct")
+ }else {
+     console.log("That answer is not correct")
+ }
+
+
+//  ===
+
+var test3 = 20
+var test4 = 40
+if(  test3 === test4){
+    console.log( "That answer is correct")
+ }else {
+     console.log("That answer is not correct")
+ }
+ 
+ 
+ 
